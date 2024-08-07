a6621973bf70fd15164bc41693ddf74f0c10df48
Wenderson Damazio <wendersonmendes60@gmail.com>
2024-08-07 14:46:46 -0300

    Altera o server.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log("Boas-vindas a Api do AllBooks")
+  console.log("API disponivel atravez da url http://localhost:8000")
 })
